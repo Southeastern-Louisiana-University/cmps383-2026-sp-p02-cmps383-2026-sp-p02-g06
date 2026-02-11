0a6fe3982e900d19da5ae77cae784cffbc6acc4c
ArmoredRed <trevorconjr@gmail.com>
2026-02-10 20:29:37 -0600

    Initial work on User and Role Dto's
    im not sure how to handle the datacontext,

---
+++ Selu383.SP26.Api/Features/User/UserRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Selu383.SP26.Api.Features.User;
+
+// keep IdentityUserRole<int> for the keys; expose navigations only
+public class UserRole : IdentityUserRole<int>
+{
+    public virtual User User { get; set; }
+    public virtual Role Role { get; set; }
+}
